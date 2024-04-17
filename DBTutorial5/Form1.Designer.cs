cb80f2f783a2ee35ebc9933d7508366d434dfee5
Maggc <Maggc>
2024-04-18 05:59:00 +0900

    Add project files.

---
+++ DBTutorial5/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace DBTutorial5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Office Code:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Staff Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contains";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(201, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(201, 250);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 301);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 112);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 159);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(405, 301);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(330, 154);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 608);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ DBTutorial5/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBTutorial5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            using (var classicContext = new classicmodelsEntities())
+            {
+                /***** Do the following program yourself *****/
+                var offCode = from list in classicContext.offices
+
+                              select list.officeCode;
+;
+
+                foreach (var offCode2 in offCode.ToList())
+                {
+                    comboBox1.Items.Add(offCode2);
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = "";
+            String temp = Convert.ToString(comboBox1.SelectedItem);
+            using (var classicContext = new classicmodelsEntities())
+            {
+                //========== Display Office Information ============
+                var offInfo = from list in classicContext.offices
+                              where list.officeCode.Contains(temp)
+                              select list;
+
+                foreach (var offInfo2 in offInfo.ToList())
+                {
+                    textBox2.Text = "Phone Number: " + offInfo2.phone + "\r\nAddress: " + offInfo2.addressLine1 + ", " + offInfo2.addressLine2 + "\r\nCity: " + offInfo2.city + "\r\nCountry: " + offInfo2.country;
+                }
+
+                comboBox2.Items.Clear();
+                textBox1.Clear();
+
+                var staffName = from list in classicContext.employees
+                                where list.officeCode == temp
+                                select new { list.firstName, list.lastName };
+
+
+                foreach (var staffName2 in staffName.ToList())
+                {
+                    comboBox2.Items.Add(staffName2.firstName + " " +staffName2.lastName);
+                }
+
+                textBox3.Clear();
+
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String temp = Convert.ToString(comboBox2.SelectedItem);
+
+            using (var classicContext = new classicmodelsEntities())
+            {
+                var staffInfo = from sInfo in classicContext.employees
+                                where (sInfo.firstName + " " + sInfo.lastName) == temp
+                       select new { sInfo.email, sInfo.extension, sInfo.jobTitle };
+
+                foreach (var staffInfo2 in staffInfo.ToList())
+                {
+                    textBox3.Text = "Email: " + staffInfo2.email + "\r\nExtension: " + staffInfo2.extension + "\r\nJob Title: " + staffInfo2.jobTitle;
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = "";
+            using (var classicContext = new classicmodelsEntities())
+            {
+                String temp = Convert.ToString(comboBox1.SelectedItem);
+                comboBox2.Items.Clear();
+                textBox3.Clear();
+
+                /* Do the following program youself */
+                var staffName = from list in classicContext.employees
+                                where list.officeCode == comboBox1.SelectedItem && list.firstName.Contains(textBox1.Text)
+                                select new { list.firstName, list.lastName };
+                    /* SELECT firstName, lastName FROM employees */
+  /* WHERE …… [Tips] - (comboBox1 selectd item = office code 
+                          && firstName conatains textbox3.text) */
+
+                foreach (var staffName2 in staffName.ToList())
+                {
+                    comboBox2.Items.Add(staffName2.firstName + " " + staffName2.lastName);
+                }
+            }
+
+        }
+    }
+}
